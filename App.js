d72487bd33910b7fac19997bbf0a70a8f571688a
Louise Jacinto <donnalouise.jacinto@benilde.edu.ph>
2020-05-23 17:45:43 +0800

    removed unnecessary styles

--- App.js
+++ App.js
@@ -12,35 +12,25 @@ import {
   StyleSheet,
   ScrollView,
   View,
-  Text,
-  StatusBar,
+  Text
 } from 'react-native';
 
 import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
+  Colors
 } from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
   return (
     <>
-      {/* <StatusBar barStyle="dark-content" /> */}
       <SafeAreaView>
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView} >
-          <Header />
-          {/* {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )} */}
+
           <View style={styles.body}>
-            <Text>QR Code Generator</Text>
+            <Text style={styles.sectionTitle} >QR Code Generator</Text>
           </View>
+
         </ScrollView>
       </SafeAreaView>
     </>
@@ -51,42 +41,17 @@ const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: Colors.lighter,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
   body: {
     backgroundColor: Colors.white,
     display: 'flex',
     justifyContent: 'center',
-    alignItems: 'center'
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+    alignItems: 'center',
   },
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
     color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  }
 });
 
 export default App;
