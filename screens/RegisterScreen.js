0440cec45d077be3b2bfbfefb800197aec7e16a5
Louise Jacinto <donnalouise.jacinto@benilde.edu.ph>
2020-05-25 00:31:32 +0800

    add qr code generator

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 import { StyleSheet } from 'react-native'
 
@@ -12,12 +12,52 @@ import {
   Text
  } from 'native-base';
 
-const RegisterScreen = () => {
+const RegisterScreen = ({navigation}) => {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [empNumber, setEmpNumer] = useState('');
   const [company, setCompany] = useState('');
-  
+  const [plateNumber, setPlateNumber] = useState('');
+  const today = new Date()
+
+  var month = new Array(12);
+  month[0] = "January";
+  month[1] = "February";
+  month[2] = "March";
+  month[3] = "April";
+  month[4] = "May";
+  month[5] = "June";
+  month[6] = "July";
+  month[7] = "August";
+  month[8] = "September";
+  month[9] = "October";
+  month[10] = "November";
+  month[11] = "December";
+
+  useEffect(() => {
+    
+    // var currentMonth = month[today.getUTCMonth()];
+    // var currentYear = today.getUTCFullYear();
+    // var currentHour = today.getUTCHours()
+    // var currentMinute = today.getUTCMinutes()
+    // console.log(currentMonth, currentDay, currentYear, currentHour, currentMinute)
+    console.log('mount')
+  }, [])
+
+  const handleSubmit = async () => {  
+    var date = month[today.getUTCMonth()]+ ' '+today.getDate()+ ', ' +today.getFullYear();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+    navigation.navigate('Home', {
+      firstName: firstName,
+      lastName: lastName,
+      empNumber: empNumber,
+      company: company,
+      plateNumber: plateNumber,
+      dateTime: date+' '+time
+    })
+  }
+
  return (
    <Container>
     <Content>
@@ -31,12 +71,15 @@ const RegisterScreen = () => {
         <Item>
           <Input placeholder="Employee Number" value={empNumber} onChangeText={(text) => setEmpNumer(text)} />
         </Item>
-        <Item last>
+        <Item>
           <Input placeholder="Company Name" value={company} onChangeText={(text) => setCompany(text)} />
         </Item>
-        <Button onPress={() => 
-          console.log(firstName, lastName, empNumber, company)
-        }>
+        <Item last>
+          <Input placeholder="Plate Number" value={plateNumber} onChangeText={(text) => setPlateNumber(text)} />
+        </Item>
+        <Button onPress={() => {
+          handleSubmit()
+        }}>
           <Text>Submit</Text>
         </Button>
       </Form>
